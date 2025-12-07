9bcbe7f5ebf2467cab8921767774f1477a54354d
DEIDRE <deidrepizarro@gmail.com>
2025-12-07 02:26:38 +0100

    Guardo cambios antes del rebase

--- git-formulario.py
+++ git-formulario.py
@@ -3,12 +3,18 @@
 
  # crear un pequeño formulario que me pregunte al usuario Nombre, apelli, edad y sexo y luego lo devuelva concatenado
 
-nombre = input("Indicame tu nombre por favor: ")
-apellido = input("Indicame tu apellido también por favor: ")
-edad= input("Indicame ahora tu edad : ")
-sexo= input("por ultimo indicame tu sexo: ")
+#nombre = input("Indicame tu nombre por favor: ")
+#apellido = input("Indicame tu apellido también por favor: ")
+#edad= input("Indicame ahora tu edad : ")
+#sexo= input("por ultimo indicame tu sexo: ")
 
-print("tu nombre es " + nombre +" , tu apellido es " + apellido + " , tienes " + edad + "años, de sexo " + sexo + "" )
+#print("tu nombre es " + nombre +" , tu apellido es " + apellido + " , tienes " + edad + "años, de sexo " + sexo + "" )
 
 
-print("modificando codigo")
+#print("modificando codigo")
+
+
+
+lista_base = [[]] * 3
+
+lista_base[0].append(1)
